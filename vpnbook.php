ab4b0d822cc3bd4c626057760ca2816b0648d34c
0px <32860414+0px0@users.noreply.github.com>
2018-07-23 04:56:11 +0000

    Update vpnbook.php

--- vpnbook.php
+++ vpnbook.php
@@ -1,4 +1,7 @@
+
 <?php
+//Author 	  : ./0px >> 0pxDarkit
+//Description  : Just Simple Source
 function bacaHTML($url){
      // inisialisasi CURL
      $data = curl_init();
@@ -10,16 +13,18 @@ function bacaHTML($url){
      curl_close($data);
      return $hasil;
 }
-$kodeHTML =  bacaHTML('www.vpnbook.com');
+$kodeHTML =  bacaHTML('https://www.vpnbook.com');
 $pecah = explode('<ul class="square">', $kodeHTML);
-$pecah2 = explode('</li>', $pecah[4]);
+$pecah2 = explode('</li>', $pecah[3]);
 
+echo "<title>Simple Grab By ./0px</title>";
 echo "<center>";
 echo "<div style='background: #9fc5e8; Position:center; color:#0000cc; padding: 20px 10px 20px 10px; text-align: center; font-family: Agency FB; width: 400px; font-size: 30px'>";
 echo "Simple Grab By ./0px<br>";
 echo "Grab From Vpnbook.com";
+echo $pecah2[7];
 echo $pecah2[8];
-echo $pecah2[9];
 echo "</div>"; //akhir stylenya
 echo "</center>";
+echo "<center>";
 ?>
